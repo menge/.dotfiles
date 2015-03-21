3f51d1cff004f2ab1f976ede395933c90c49cc35
Emil Meng <emil.meng@gmail.com>
2015-03-21 15:08:54 -0600

    basic functionality for bootstrapping dotfiles

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,10 +9,49 @@ DOTFILES_ROOT=$(dirname ${SCRIPT_DIR})
 exclude_paths=( .git scripts )
 
 # create query for adding paths to exclude from linking
-exclude_query="-not -path ${DOTFILES_ROOT}" # remove dotfiles directory, but not files
+exclude_query="-not -path ." # remove dotfiles directory, but not files
 for path in ${exclude_paths[*]}
 do
-    exclude_query="${exclude_query} -not -path ${DOTFILES_ROOT}/${path} -not -path ${DOTFILES_ROOT}/${path}/*  "
+    exclude_query="${exclude_query} -not -path ./${path} -not -path ./${path}/*  "
 done
 
-find ${DOTFILES_ROOT} ${exclude_query} -type f
+echo "================================================================================"
+echo "Creating Directories"
+echo "================================================================================"
+dirs=$( (cd ${DOTFILES_ROOT} ; find . ${exclude_query} -type d -printf "%P\n" | sort) )
+for dir in ${dirs}
+do
+    if [ -d "${HOME}/${dir}" ]
+    then
+        echo "${HOME}/${dir}"
+    else
+        echo "${HOME}/${dir}: Making missing directory"
+        mkdir -p ${HOME}/${dir}
+    fi
+done
+
+echo "================================================================================"
+echo "Linking Dotfiles"
+echo "================================================================================"
+dotfiles=$( (cd ${DOTFILES_ROOT} ; find . ${exclude_query} -type f -printf "%P\n" | sort) )
+for dotfile in ${dotfiles}
+do
+    #(cd ${DOTFILES_ROOT} ; ln -s ${dotfile} $HOME
+
+    linkfile=$(readlink -f $HOME/$dotfile)
+    if [ -e ${linkfile} ]
+    then
+        realfile=$(cd ${DOTFILES_ROOT}; readlink -f ${dotfile})
+        if [ ${linkfile} == ${realfile} ]
+        then
+            echo "${dotfile}"
+        else
+            echo "Creating link for ${dotfile}"
+            rm -f ${linkfile}
+            (cd ${DOTFILES_ROOT}; ln -s $(readlink -f ${dotfile}) ${linkfile})
+        fi
+    else
+        echo "Creating link for ${dotfile}"
+        (cd ${DOTFILES_ROOT}; ln -s $(readlink -f ${dotfile}) ${linkfile})
+    fi
+done
