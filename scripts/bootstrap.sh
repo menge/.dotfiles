7e970dcdf6734518e8858490fb1aa09e0b85a3f1
Emil Meng <emil.meng@gmail.com>
2015-03-21 14:15:54 -0600

    Find now excludes paths we specify

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,6 +1,18 @@
-#!/bin/bash -x
+#!/bin/bash
+
+set -f # disable globbing to pass '*' into find command later
 
 THIS_SCRIPT=$(readlink -f $0)
 SCRIPT_DIR=$(dirname ${THIS_SCRIPT})
 DOTFILES_ROOT=$(dirname ${SCRIPT_DIR})
 
+exclude_paths=( .git scripts )
+
+# create query for adding paths to exclude from linking
+exclude_query="-not -path ${DOTFILES_ROOT}" # remove dotfiles directory, but not files
+for path in ${exclude_paths[*]}
+do
+    exclude_query="${exclude_query} -not -path ${DOTFILES_ROOT}/${path} -not -path ${DOTFILES_ROOT}/${path}/*  "
+done
+
+find ${DOTFILES_ROOT} ${exclude_query} -type f
