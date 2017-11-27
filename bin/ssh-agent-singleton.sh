b84da40a63af18301eb0ff3c19e69572e926f432
Emil Meng <emil.meng@gmail.com>
2017-11-26 21:46:48 -0700

    making ssh-agent work for lubuntu environment

--- bin/ssh-agent-singleton.sh
+++ bin/ssh-agent-singleton.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-agent_sock=$(find /tmp/ssh-* -user ${USER} -type s 2> /dev/null)
-agent_pid=$(pgrep -u ${USER} '^ssh-agent$')
+agent_sock=$(find /tmp/ssh-* -user ${USER} -type s  | head -n1 2> /dev/null)
 
 if [ "${agent_sock}" == "" ]
 then
     ssh-agent | grep -v "Agent pid"
 else
     echo "SSH_AUTH_SOCK=$agent_sock; export SSH_AUTH_SOCK"
-    echo "SSH_AGENT_PID=$agent_pid; export SSH_AGENT_PID"
 fi
